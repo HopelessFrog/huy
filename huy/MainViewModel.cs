a2fd97bb7370889a83cb126f5aecf67439996132
leahalarsanik@gmail.com <leahalarsanik@gmail.com>
2023-11-08 22:02:06 +0300

    Add project files.

---
+++ huy/MainViewModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+using DevExpress.Mvvm;
+using huy.Pages;
+
+namespace huy
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private Page currentPage;
+        public Page CurrentPage 
+        {
+            get
+            {
+                return currentPage;
+            }
+            set
+            {
+                if (value != currentPage)
+                {
+                    currentPage = value;
+                    RaisePropertiesChanged(nameof(CurrentPage));
+                }
+            }
+        }
+
+        private Page page1 = new Pages.Page1();
+
+        private Page page2 = new Pages.Page2();
+
+        public MainViewModel()
+        {
+            page1.DataContext = new Page1VM();
+            page2.DataContext = new Page2VM();
+        }
+
+        public ICommand Page1Command
+        {
+            get
+            {
+
+                return new DelegateCommand(() =>
+                {
+                    CurrentPage = page1;
+                });
+
+            }
+        }
+        public ICommand Page2Command
+        {
+            get
+            {
+
+                return new DelegateCommand(() =>
+                {
+                    CurrentPage = page2;
+                });
+
+            }
+        }
+
+    }
+}
---
+++ huy/Pages/Page2VM.cs
@@ -0,0 +1,27 @@
+﻿using DevExpress.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace huy.Pages
+{
+    public  class Page2VM
+    {
+        public ICommand NothingHappen
+        {
+            get
+            {
+
+                return new DelegateCommand(() =>
+                {
+                    MessageBox.Show("Nothing Happen");
+                });
+
+            }
+        }
+    }
+}
